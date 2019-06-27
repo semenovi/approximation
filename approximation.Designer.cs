4d34fc981d47a2a33252fba946d8e1d40da3165d
semenovi <t79601712352@gmail.com>
2019-06-27 07:48:34 +0300

    init

---
+++ approximation.Designer.cs
@@ -0,0 +1,124 @@
+﻿namespace approximation
+{
+    partial class approximation
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.input_x_t = new System.Windows.Forms.TextBox();
+            this.output_t = new System.Windows.Forms.TextBox();
+            this.linear = new System.Windows.Forms.Button();
+            this.input_y_t = new System.Windows.Forms.TextBox();
+            this.x_l = new System.Windows.Forms.Label();
+            this.y_l = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // input_x_t
+            // 
+            this.input_x_t.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.input_x_t.Location = new System.Drawing.Point(17, 1);
+            this.input_x_t.Name = "input_x_t";
+            this.input_x_t.Size = new System.Drawing.Size(265, 13);
+            this.input_x_t.TabIndex = 0;
+            // 
+            // output_t
+            // 
+            this.output_t.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.output_t.Location = new System.Drawing.Point(2, 62);
+            this.output_t.Name = "output_t";
+            this.output_t.ReadOnly = true;
+            this.output_t.Size = new System.Drawing.Size(280, 13);
+            this.output_t.TabIndex = 3;
+            // 
+            // linear
+            // 
+            this.linear.Location = new System.Drawing.Point(2, 36);
+            this.linear.Name = "linear";
+            this.linear.Size = new System.Drawing.Size(280, 23);
+            this.linear.TabIndex = 2;
+            this.linear.Text = "linear";
+            this.linear.UseVisualStyleBackColor = true;
+            this.linear.Click += new System.EventHandler(this.calculate_b_Click);
+            // 
+            // input_y_t
+            // 
+            this.input_y_t.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.input_y_t.Location = new System.Drawing.Point(17, 20);
+            this.input_y_t.Name = "input_y_t";
+            this.input_y_t.Size = new System.Drawing.Size(265, 13);
+            this.input_y_t.TabIndex = 1;
+            // 
+            // x_l
+            // 
+            this.x_l.AutoSize = true;
+            this.x_l.Location = new System.Drawing.Point(3, 1);
+            this.x_l.Name = "x_l";
+            this.x_l.Size = new System.Drawing.Size(12, 13);
+            this.x_l.TabIndex = 4;
+            this.x_l.Text = "x";
+            // 
+            // y_l
+            // 
+            this.y_l.AutoSize = true;
+            this.y_l.Location = new System.Drawing.Point(3, 20);
+            this.y_l.Name = "y_l";
+            this.y_l.Size = new System.Drawing.Size(12, 13);
+            this.y_l.TabIndex = 5;
+            this.y_l.Text = "y";
+            // 
+            // approximation
+            // 
+            this.AcceptButton = this.linear;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 77);
+            this.Controls.Add(this.y_l);
+            this.Controls.Add(this.x_l);
+            this.Controls.Add(this.input_y_t);
+            this.Controls.Add(this.linear);
+            this.Controls.Add(this.output_t);
+            this.Controls.Add(this.input_x_t);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MaximizeBox = false;
+            this.Name = "approximation";
+            this.ShowIcon = false;
+            this.Text = "approximation";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox input_x_t;
+        private System.Windows.Forms.TextBox output_t;
+        private System.Windows.Forms.Button linear;
+        private System.Windows.Forms.TextBox input_y_t;
+        private System.Windows.Forms.Label x_l;
+        private System.Windows.Forms.Label y_l;
+    }
+}
+
---
+++ approximation.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace approximation
+{
+    public partial class approximation : Form
+    {
+        Int32[] input_x = new Int32[256];
+        Int32[] input_y = new Int32[256];
+        Int32 input_size = 0;
+        float[] output = new float[256];
+        float a = 0;
+        float b = 0;
+
+        public approximation()
+        {
+            InitializeComponent();
+        }
+
+        private void input_to_arrays(String x, String y)
+        {
+            int last_separator = -1;
+            input_size = 0;
+            for (int i = 0; i < x.Length; i++)
+            {
+                if (x[i] == ',')
+                {
+                    input_x[input_size] = System.Convert.ToInt32(
+                        x.Substring(last_separator + 1, i - last_separator - 1));
+                    last_separator = i;
+                    input_size = input_size + 1;
+                }
+                if (x.Length == i + 1)
+                {
+                    input_x[input_size] = System.Convert.ToInt32(
+                        x.Substring(last_separator + 1, i - last_separator));
+                    input_size = input_size + 1;
+                }
+            }
+            last_separator = -1;
+            int n = 0;
+            for (int i = 0; i < y.Length; i++)
+            {
+                if (y[i] == ',')
+                {
+                    input_y[n] = System.Convert.ToInt32(
+                        y.Substring(last_separator + 1, i - last_separator - 1));
+                    last_separator = i;
+                    n = n + 1;
+                }
+                if (y.Length == i + 1)
+                {
+                    input_y[n] = System.Convert.ToInt32(
+                        y.Substring(last_separator + 1, i - last_separator));
+                    n = n + 1;
+                }
+            }
+        }
+
+        private void calculate_a_b()
+        {
+            float sigma_x_y = 0;
+            float sigma_x_2 = 0;
+            float sigma_x = 0;
+            float sigma_y = 0;
+            for (int i = 0; i < input_size; i++)
+            {
+                sigma_x_y = sigma_x_y + (input_x[i] * input_y[i]);
+                sigma_x_2 = sigma_x_2 + (input_x[i] * input_x[i]);
+                sigma_x = sigma_x + input_x[i];
+                sigma_y = sigma_y + input_y[i];
+            }
+
+            a = (input_size * sigma_x_y - sigma_x * sigma_y) / (input_size * sigma_x_2 - sigma_x * sigma_x);
+            b = (sigma_y - a * sigma_x) / input_size;
+        }
+        
+        private void calculate_linear_approximation()
+        {
+            for (int i = 0; i < input_size; i++)
+            {
+                output[i] = input_y[i] - (a * input_x[i] + b);
+            }
+        }
+
+        private void calculate_b_Click(object sender, EventArgs e)
+        {
+            if (input_x_t.Text == "")
+            {
+                output_t.Text = "array 'x' is empty";
+            }
+            else if(input_y_t.Text == "")
+            {
+                output_t.Text = "array 'y' is empty";
+            }
+            else
+            {
+                input_to_arrays(input_x_t.Text, input_y_t.Text);
+                calculate_a_b();
+                calculate_linear_approximation();
+                String output_s = "";
+                output_s = output_s + "a = " + a.ToString() + ", ";
+                output_s = output_s + "b = " + b.ToString() + ", ";
+                output_s = output_s + "yi-(axi+b) = { ";
+                for (int i = 0; i < input_size; i++)
+                {
+                    output_s = output_s + output[i].ToString();
+                    if (i != input_size - 1)
+                    {
+                        output_s = output_s + ", ";
+                    }
+                }
+                output_s = output_s + " }";
+                output_t.Text = output_s;
+            }
+        }
+    }
+}
